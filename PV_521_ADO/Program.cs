d017892feae75a1b367f97dad4e9d780e9224c20
User <User@DESKTOP-QHG18FL>
2026-03-23 22:18:23 +0300

    GetPrimaryKeyColumnName

--- PV_521_ADO/Program.cs
+++ PV_521_ADO/Program.cs
@@ -16,7 +16,18 @@ namespace PV_521_ADO
 
 			Connector connector = new Connector(connection_string);
 
-			connector.Insert("INSERT Directors (first_name,last_name) VALUES (N'Guy', N'Richie');");
+			Console.WriteLine(connector.GetPrimaryKeyColumnName("Directors"));
+			Console.WriteLine(connector.GetPrimaryKeyColumnName("Movies"));
+
+			//			connector.Insert($@"INSERT Directors (director_id,first_name,last_name) 
+			//VALUES ({connector.GetNextPrimaryKey("Directors")}, N'Guy', N'Richie');");
+
+			connector.Insert
+			(
+				"Directors",
+				"director_id,first_name,last_name",
+				$"{connector.GetNextPrimaryKey("Directors")},John,Singleton"
+			);
 
 			Console.WriteLine($"PK Max:\t{connector.GetMaxPrimaryKey("Directors")}");
 
