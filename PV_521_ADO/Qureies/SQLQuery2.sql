cdbfb4c8b7d08b15fed3ad2e5ca72a91b8129455
User <User@DESKTOP-QHG18FL>
2026-03-25 20:15:15 +0300

    Connector DLL DONE

---
+++ PV_521_ADO/Qureies/SQLQuery2.sql
@@ -0,0 +1,2 @@
+﻿IF NOT EXISTS (SELECT director_id FROM Directors WHERE  first_name=N'John' AND last_name=N'Singleton' )
+INSERT Directors(director_id,first_name,last_name) VALUES (N'16'N'John',N'Singleton')
