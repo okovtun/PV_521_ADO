f7295c48a9824fdf5748198278a7a4fec21e41b0
User <User@DESKTOP-QHG18FL>
2026-03-27 20:27:25 +0300

    Directions in progress

---
+++ Academy/MainForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace Academy
+{
+	public partial class MainForm : Form
+	{
+		DBtools.Connector connector;
+		public MainForm()
+		{
+			InitializeComponent();
+			connector = new DBtools.Connector(ConfigurationManager.ConnectionStrings["PV_521_Import"].ConnectionString);
+			dgvDirections.DataSource = connector.Select("*", "Directions");
+		}
+	}
+}
