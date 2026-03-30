a03d91b13d973ddac7f7ffb4f073a37dcb929691
User <User@DESKTOP-QHG18FL>
2026-03-30 21:43:07 +0300

    Filters in progress.

--- Academy/MainForm.cs
+++ Academy/MainForm.cs
@@ -82,7 +82,13 @@ namespace Academy
 
 		private void cbStudentsDirection_SelectedIndexChanged(object sender, EventArgs e)
 		{
-
+			d_groups = connector.GetDictionary
+				(
+				"Groups",
+				$"direction={d_directions[cbStudentsDirection.SelectedItem.ToString()]}"
+				);
+			cbStudentsGroup.Items.Clear();
+			cbStudentsGroup.Items.AddRange(d_groups.Keys.ToArray());
 			dgvStudents.DataSource = connector.Select
 				(
 					queries[0].ToString()+
